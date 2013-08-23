c2c0c0becfe7b7e2f1a510facae93d9674fe7d3a
pocesar <gahgneh+github@gmail.com>
2013-08-23 11:57:39 -0300

    wip

--- examples/static/browser.js
+++ examples/static/browser.js
@@ -3,11 +3,22 @@
 
   var App = angular.module('DemoApp', ['angularPrimus']);
 
-  var Main = App.controller('Main', ['$scope', '$primus', function($scope, $primus){
+  var Main = App.controller('Main', ['$scope', '$primus', '$timeout', function($scope, $primus, $timeout){
     $scope.$primus = $primus($scope);
     $scope.history = [];
 
-    $scope.$primus.watch();
+    $scope.$primus.model(['history']); // the scope variable must exist before
+
+    // Don't allow arbitrary updating server data like this,
+    // this is an example only
+    $timeout(function(){
+      $scope.history.push({}); // this will update
+    }, 1000);
+
+    $timeout(function(){
+      $scope.history.push({}); // this will update
+    }, 1000);
+
   }]);
 
   Main.directive('main', [function(){
